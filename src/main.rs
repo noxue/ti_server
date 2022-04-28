27ab362bf9c3cbcee3e9a05f633a471e2ba11fc9
noxue <173126019@qq.com>
2022-04-28 23:41:38 +0800

    tokio

---
+++ src/main.rs
@@ -0,0 +1,55 @@
+use ti_protocol::{get_header_size, PackType, PacketHeader, Task, TaskResult, TiUnPack};
+use tokio::{
+    io::AsyncReadExt,
+    net::{TcpListener, TcpStream},
+};
+
+async fn handle_connection(mut stream: TcpStream) {
+    let len: usize = get_header_size();
+    print!("header size:{}", len);
+    loop {
+        // thread::sleep(Duration::from_millis(10));
+
+        let mut header = vec![0; len];
+        stream.read(&mut header).await.unwrap();
+        let header = PacketHeader::unpack(&header).unwrap();
+
+        // 检查标志位，不对就跳过
+        if !header.check_flag() {
+            continue;
+        }
+
+        // 根据包头长度读取数据
+        let mut body = vec![0; header.body_size as usize];
+        stream.read(&mut body).await.unwrap();
+
+        // 根据数据类型解析数据
+        match header.pack_type {
+            PackType::Task => {
+                let task = Task::unpack(&body).unwrap();
+                println!("{:?}", task);
+            }
+            PackType::TaskResult => {
+                let task = TaskResult::unpack(&body).unwrap();
+                println!("{:?}", task);
+            }
+        }
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    // 监听127.0.0.1:8000
+    let listener = TcpListener::bind("0.0.0.0:8000").await.unwrap();
+    loop {
+        match listener.accept().await {
+            Ok((_socket, addr)) => {
+                tokio::spawn(async move {
+                    handle_connection(_socket).await;
+                });
+            }
+            Err(e) => println!("couldn't get client: {:?}", e),
+        }
+        println!("一次请求完成");
+    }
+}
